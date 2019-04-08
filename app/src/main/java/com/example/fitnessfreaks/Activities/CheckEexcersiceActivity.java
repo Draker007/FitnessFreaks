c940a3249b919c90c6196f1ceed0750968550f61
Deepak singh <drakerdeepak@gmail.com>
2019-04-08 16:31:09 +0530

    check Excersice added

--- app/src/main/java/com/example/fitnessfreaks/Activities/CheckEexcersiceActivity.java
+++ app/src/main/java/com/example/fitnessfreaks/Activities/CheckEexcersiceActivity.java
@@ -1,15 +1,54 @@
 package com.example.fitnessfreaks.Activities;
 
+import android.annotation.SuppressLint;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
+import com.example.fitnessfreaks.Adapters.checkExcersiceAdapter;
+import com.example.fitnessfreaks.Fragments.descriptionexcersice_fragment;
+import com.example.fitnessfreaks.Fragments.instructionexcersice_fragment;
 import com.example.fitnessfreaks.R;
 
 public class CheckEexcersiceActivity extends AppCompatActivity {
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private checkExcersiceAdapter adapter;
+    TextView excersiceName;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_eexcersice);
+
+        Initialize();
+        clickListeners();
+        fragments();
+    }
+
+
+    public void Initialize(){
+         tabLayout = (TabLayout)findViewById(R.id.checkExcersiceTabLayout);
+         viewPager=(ViewPager)findViewById(R.id.checkExcersiceViewer);
+         excersiceName=(TextView)findViewById(R.id.exerciseName);
+         adapter = new checkExcersiceAdapter(getSupportFragmentManager());
+
+    }
+
+    public void clickListeners(){
+
+
+
+    }
+
+    public void fragments(){
+    adapter.AddFragment(new descriptionexcersice_fragment(),"Description");
+    adapter.AddFragment(new instructionexcersice_fragment(),"Instructions");
+    viewPager.setAdapter(adapter);
+    tabLayout.setupWithViewPager(viewPager);
+
     }
 }
---
+++ app/src/main/java/com/example/fitnessfreaks/Fragments/instructionexcersice_fragment.java
@@ -0,0 +1,22 @@
+package com.example.fitnessfreaks.Fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.fitnessfreaks.R;
+
+public class instructionexcersice_fragment extends Fragment {
+    View v;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        v = inflater.inflate(R.layout.instructionexcersice_fragment,container,false);
+        return v;
+    }
+}
